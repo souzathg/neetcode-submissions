eec9f837ca858aaf482e69e13891c618e3b33eba
Thiago Tioma <thiago.tioma@outlook.com>
2026-07-18 22:36:55 -0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+
+        int[] charFreq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            charFreq[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            charFreq[c - 'a']--;
+        }
+
+        for (int i : charFreq) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
